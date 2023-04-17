6ec51de71c78e82d20e4e3f45a9fceb7e8b41643
iDoyoung <ido.young@icloud.com>
2023-04-17 16:39:26 +0900

    Initial Commit

---
+++ MyAppsOperator/MyAppsOperatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyAppsOperatorApp.swift
+//  MyAppsOperator
+//
+//  Created by Doyoung on 2023/04/17.
+//
+
+import SwiftUI
+
+@main
+struct MyAppsOperatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
