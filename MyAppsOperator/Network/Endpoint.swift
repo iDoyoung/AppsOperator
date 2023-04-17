4778dc6316acb4380eaaeac102c36c5e3f0c7923
iDoyoung <ido.young@icloud.com>
2023-04-17 17:04:19 +0900

    Implement Endpoint

---
+++ MyAppsOperator/Network/Endpoint.swift
@@ -0,0 +1,29 @@
+//
+//  Endpoint.swift
+//  MyAppsOperator
+//
+//  Created by Doyoung on 2023/04/17.
+//
+
+import Foundation
+
+struct Endpoint<R>: ResponseRequester {
+    
+    typealias Response = R
+    
+    var path: String
+    var isFullPath: Bool
+    var method: HTTPMethodType
+    var headerParameters: [String : String]
+    var queryParameters: [String : String]
+    var bodyParameters: [String : Any]
+    
+    init(path: String, isFullPath: Bool = false, method: HTTPMethodType = .get, headerParameters: [String: String] = [:], queryParameters: [String: String] = [:], bodyParameters: [String: Any] = [:]) {
+        self.path = path
+        self.isFullPath = isFullPath
+        self.method = method
+        self.headerParameters = headerParameters
+        self.queryParameters = queryParameters
+        self.bodyParameters = bodyParameters
+    }
+}
