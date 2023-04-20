55cbb88e5ab944c0d08b08602b40a408d4fb929c
iDoyoung <ido.young@icloud.com>
2023-04-20 14:05:31 +0900

    Add APIEndpoints Of Create Announcement

---
+++ MyAppsOperator/Network/APIEndpoints.swift
@@ -0,0 +1,15 @@
+//
+//  APIEndpoints.swift
+//  MyAppsOperator
+//
+//  Created by Doyoung on 2023/04/19.
+//
+
+import Foundation
+
+struct APIEndpoints {
+    
+    static func createAnnouncement() -> Endpoint<Announcement> {
+       return Endpoint(path: "")
+    }
+}
