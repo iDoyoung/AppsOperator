f0ccaeaadcbda89a473ba1ce6e993cd010ebdbc3
iDoyoung <ido.young@icloud.com>
2023-04-24 17:04:27 +0900

    Add Create Announcement Alert Model

---
+++ MyAppsOperator/Scenes/Announcement/CreateAnnouncementAlert.swift
@@ -0,0 +1,13 @@
+//
+//  CreateAnnouncementAlert.swift
+//  MyAppsOperator
+//
+//  Created by Doyoung on 2023/04/24.
+//
+
+import Foundation
+
+struct CreateAnnouncementAlert {
+    let title: String
+    let error: String
+}
