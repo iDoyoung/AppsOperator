0b230819c16a5ee74a85b3ff402f848440ddc42b
iDoyoung <ido.young@icloud.com>
2023-04-26 15:32:13 +0900

    Implement Configure

--- MyAppsOperator/Scenes/Announcement/CreateAnnouncementView.swift
+++ MyAppsOperator/Scenes/Announcement/CreateAnnouncementView.swift
@@ -7,55 +7,97 @@
 
 import SwiftUI
 
-struct CreateAnnouncementView: View {
-    
+protocol CreateAnnouncementViewState {
+    var createAlert: CreateAnnouncementErrorAlert { get }
+}
+
+struct CreateAnnouncementView: View, CreateAnnouncementViewState {
+   
     enum Field: Hashable {
         case title
         case content
     }
     
-    @State private var title = ""
-    @State private var content = ""
-    let contentPlaceholder = "여기에 공지할 내용을 작성하세요."
+    // Components
+    
+    private var announcementWorker = AnnouncementWorker(
+        network: NetworkDataCodableService(
+            network: NetworkService(
+                configuration: NetworkAPIConfiguration(
+                    baseURL: URL(string: "http://127.0.0.1:8080")!
+                )
+            )
+        )
+    )
+    
+    var interactor: CreateAnnouncementInteractor
+    @State var model = AnnouncementViewModel()
+    @StateObject var createAlert: CreateAnnouncementErrorAlert
+    
+    // UI
     
     @FocusState private var focusedField: Field?
+    let contentPlaceholder = "여기에 공지할 내용을 작성하세요."
     
     var body: some View {
         VStack {
             TextField(
                 "제목",
-                text: $title
+                text: $model.title
             )
+            .tint(.red)
             .font(.title)
             .focused($focusedField, equals: .title)
             .onSubmit {
                 focusedField = .content
             }
             
-            TextEditor(text: $content)
+            TextEditor(text: $model.content)
                 .scrollContentBackground(.hidden)
                 .focused($focusedField, equals: .content)
                 .background(alignment: .topLeading) {
-                    TextEditor(text: .constant(content.isEmpty ? contentPlaceholder : ""))
+                    TextEditor(text: .constant(model.content.isEmpty ? contentPlaceholder : ""))
                         .foregroundColor(.gray)
                 }
-            Button(action: createAnnouncement) {
-                Text("공치하기")
+            Button {
+                Task {
+                        try await interactor.create(
+                            title: model.title,
+                            content: model.content
+                        )
+                }
+            } label: {
+                Text("공지하기")
                     .frame(maxWidth: .infinity)
             }
             .tint(.red)
             .buttonStyle(.borderedProminent)
             .padding([.leading, .trailing], 5.0)
             .controlSize(.large)
+            .alert(
+                createAlert.title,
+                isPresented: $createAlert.didError,
+                presenting: createAlert
+            ) { _ in
+            } message: { alert in
+                Text(alert.content)
+            }
         }
         .onAppear {
             focusedField = .title
         }
         .padding()
     }
-    
-    func createAnnouncement() {
-        print("POST TO SERVER")
+ 
+    init() {
+        let interactor = CreateAnnouncementInteractor()
+        var stateController = CreateAnnouncementStateController()
+        let createAnnouncementErrorAlert = CreateAnnouncementErrorAlert()
+        stateController.createAlert = createAnnouncementErrorAlert
+        _createAlert = StateObject(wrappedValue: { createAnnouncementErrorAlert }())
+        interactor.worker = announcementWorker
+        interactor.stateController = stateController
+        self.interactor = interactor
     }
 }
 
