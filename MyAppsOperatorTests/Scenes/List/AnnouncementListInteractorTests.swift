5fccf357e90d4ea342ee65bdcaf377e95e3f28c5
iDoyoung <ido.young@icloud.com>
2023-04-27 13:34:04 +0900

    Test Announcement List Interactorr

---
+++ MyAppsOperatorTests/Scenes/List/AnnouncementListInteractorTests.swift
@@ -0,0 +1,58 @@
+//
+//  AnnouncementListInteractorTests.swift
+//  MyAppsOperatorTests
+//
+//  Created by Doyoung on 2023/04/27.
+//
+
+import XCTest
+@testable import MyAppsOperator
+
+final class AnnouncementListInteractorTests: XCTestCase {
+    
+    //MARK: - System Under Test
+    
+    var sut: AnnouncementListInteractor!
+    
+    override func setUpWithError() throws {
+        try super.setUpWithError()
+        sut = AnnouncementListInteractor()
+        workerSpy = WorkerSpy()
+    }
+
+    override func tearDownWithError() throws {
+        sut = nil
+        workerSpy = nil
+        try super.tearDownWithError()
+    }
+    
+    //MARK: - TestDoubles
+    var workerSpy: WorkerSpy!
+    final class WorkerSpy: AnnouncementWorkerProtocol {
+        
+        var fetchCalled = false
+        var fetched = [Announcement]()
+        
+        func fetch() async throws -> [MyAppsOperator.Announcement] {
+            fetchCalled = true
+            return fetched
+        }
+        
+        func create(_ annoucement: MyAppsOperator.Announcement) async throws -> MyAppsOperator.Announcement {
+            return annoucement
+        }
+    }
+    
+    //MARK: - Tests
+    func test_fetchList() async throws {
+        // given
+        sut.worker = workerSpy
+        workerSpy.fetched = [Announcement(title: "Test", content: "Test Of Unit Test")]
+        
+        // when
+        try await sut.fetchList()
+        
+        // then
+        XCTAssertTrue(workerSpy.fetchCalled)
+    }
+}
