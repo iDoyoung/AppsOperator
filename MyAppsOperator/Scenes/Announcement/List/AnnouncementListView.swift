43840ffd968bab150399edef6bfc68f8fc4a6da1
iDoyoung <ido.young@icloud.com>
2023-04-27 10:57:48 +0900

    Add Announcement List View

---
+++ MyAppsOperator/Scenes/Announcement/List/AnnouncementListView.swift
@@ -0,0 +1,29 @@
+//
+//  AnnouncementListView.swift
+//  MyAppsOperator
+//
+//  Created by Doyoung on 2023/04/26.
+//
+
+import SwiftUI
+
+struct AnnouncementListView: View {
+    
+    @StateObject var fetchedAnnouncements = FetchedAnnouncementList()
+    
+    var body: some View {
+        NavigationStack {
+            List(fetchedAnnouncements.items) {
+                Text($0.title)
+                NavigationLink($0.title, value: $0)
+            }
+            .navigationTitle("공지사항")
+        }
+    }
+}
+
+struct AnnouncementListView_Previews: PreviewProvider {
+    static var previews: some View {
+        AnnouncementListView()
+    }
+}
