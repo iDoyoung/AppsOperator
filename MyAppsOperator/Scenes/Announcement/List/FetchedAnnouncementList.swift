f92a9c9bc8893656408f035f1df09fe9f34f45be
iDoyoung <ido.young@icloud.com>
2023-04-27 10:57:18 +0900

    Add Fetched AnnouncementList

---
+++ MyAppsOperator/Scenes/Announcement/List/FetchedAnnouncementList.swift
@@ -0,0 +1,12 @@
+//
+//  FetchedAnnouncementList.swift
+//  MyAppsOperator
+//
+//  Created by Doyoung on 2023/04/26.
+//
+
+import Foundation
+
+final class FetchedAnnouncementList: ObservableObject {
+    var items: [AnnouncementViewModel] = []
+}
