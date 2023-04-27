f67723432398df85fb08ef5db40edde4bcf7ab3a
iDoyoung <ido.young@icloud.com>
2023-04-27 13:44:01 +0900

    Add Announcement List State Controller

---
+++ MyAppsOperator/Scenes/Announcement/List/AnnouncementListStateController.swift
@@ -0,0 +1,19 @@
+//
+//  AnnouncementListStateController.swift
+//  MyAppsOperator
+//
+//  Created by Doyoung on 2023/04/27.
+//
+
+import Foundation
+
+protocol AnnouncementListStateUpdater {
+    func updateFetchedAnnouncements(_ list: [Announcement])
+}
+
+final class AnnouncementListStateController {
+    
+    func updateFetchedAnnouncements(_ list: [Announcement]) {
+        
+    }
+}
