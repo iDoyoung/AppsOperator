2eecdadba15d41d659e8d46e672cb2d865d9a584
iDoyoung <ido.young@icloud.com>
2023-05-02 14:11:33 +0900

    Implement Components

--- MyAppsOperator/Scenes/Announcement/List/AnnouncementListView.swift
+++ MyAppsOperator/Scenes/Announcement/List/AnnouncementListView.swift
@@ -9,21 +9,46 @@ import SwiftUI
 
 struct AnnouncementListView: View {
     
+    // Components
+    
+    private var announcementWorker = AnnouncementWorker(
+        network: NetworkDataCodableService(
+            network: NetworkService(
+                configuration: NetworkAPIConfiguration(
+                    baseURL: URL(string: "http://127.0.0.1:8080/announcements")!
+                )
+            )
+        )
+    )
+    var interactor: AnnouncementListInteractor
+    // UI
+    
     @StateObject var fetchedAnnouncements: FetchedAnnouncementList
     
     var body: some View {
         NavigationStack {
             List(fetchedAnnouncements.items) {
-                Text($0.title)
                 NavigationLink($0.title, value: $0)
             }
             .navigationTitle("공지사항")
+        }
+        .onAppear {
+            Task {
+                try await interactor.fetchList()
+            }
         }
     }
     
     init() {
         let fetchedAnnouncements = FetchedAnnouncementList()
+        let interactor = AnnouncementListInteractor()
+        let stateController = AnnouncementListStateController()
+        stateController.fetchedAnnouncements = fetchedAnnouncements
+        interactor.stateController = stateController
+        
         _fetchedAnnouncements = StateObject(wrappedValue: { fetchedAnnouncements }())
+        interactor.worker = announcementWorker
+        self.interactor = interactor
     }
 }
 
